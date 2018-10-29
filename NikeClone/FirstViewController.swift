b638636af2f0af2d944ec082b77100e93f97e5ab
Nathan <nathan.tugwell@me.com>
2018-10-29 13:31:22 +0000

    Initial Commit

---
+++ NikeClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NikeClone
+//
+//  Created by Nathan Tugwell on 29/10/2018.
+//  Copyright © 2018 petsathome. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ NikeClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NikeClone
+//
+//  Created by Nathan Tugwell on 29/10/2018.
+//  Copyright © 2018 petsathome. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
