f37c6e08cb47b4fe900eced3fbabc1c1227edb1a
Nathan <nathan.tugwell@me.com>
2018-10-30 09:29:31 +0000

    set up UI for the logVC

--- NikeClone/RunLogViewController.swift
+++ NikeClone/RunLogViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  RunLogViewController.swift
 //  NikeClone
 //
 //  Created by Nathan Tugwell on 29/10/2018.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class RunLogViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
